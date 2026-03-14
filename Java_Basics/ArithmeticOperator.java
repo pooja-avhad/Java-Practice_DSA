7941694086f2bdd574901058c187e5c390dc9020
pooja-avhad <avhadpooja408@gmail.com>
2026-03-14 10:20:17 +0530

    Add files via upload

---
+++ Java_Basics/ArithmeticOperator.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperator
+{
+    public static void main(String args[])
+    {
+        int a=39;
+        int b=34;
+        
+
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:" +(a-b));
+        System.out.println("Multiplication:" + (a*b));
+        System.out.println("Division:" + (a/b));
+    }
+}
