743691e2616cb9fb61a807156715c55cc09434c1
pooja-avhad <avhadpooja408@gmail.com>
2026-03-17 01:09:39 +0530

    Added Char Casting Example

---
+++ Java_Basics/TypeCasting/CharCasting.java
@@ -0,0 +1,11 @@
+public class CharCasting 
+{
+    public static void main(String[] args) 
+    {
+        //Char Casting
+        char ch='A';
+        int a=ch;
+
+        System.out.println(a);
+    }
+}
