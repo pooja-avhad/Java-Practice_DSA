bc3aa78401641488357e3c90fafd9231ac848e1a
pooja-avhad <avhadpooja408@gmail.com>
2026-03-17 01:08:24 +0530

    Added Explicit Type Casting Example

---
+++ Java_Basics/TypeCasting/ExplicitCasting.java
@@ -0,0 +1,10 @@
+public class ExplicitCasting 
+{
+    public static void main (String args[])
+    {
+        //Explicit Casting
+        double a = 4.5;
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
