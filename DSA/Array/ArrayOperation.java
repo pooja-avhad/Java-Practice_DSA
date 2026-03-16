86c66231498f9a27868909fa24e19608f3eb43a8
pooja-avhad <avhadpooja408@gmail.com>
2026-03-17 01:12:19 +0530

    Added Array Operation Example

--- DSA/Array/ArrayOperation.java
+++ DSA/Array/ArrayOperation.java
@@ -1,1 +1,28 @@
+public class ArrayOperation {
+    public static void main(String[] args) {
+        int[] arr={1,3,4,5,};
+        int sum=0;
+        int max=arr[0];
+        int min=arr[0];
 
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+
+            if(arr[i]> max)
+            {
+               max=arr[i];
+            }
+
+            if(arr[i]< min)
+             {
+                min=arr[i];
+            }
+        }
+        System.out.println("Length:"+ arr.length);
+        System.out.println("Sum:"+sum);
+        System.out.println("Largest:"+ max);
+        System.out.println("Smallest:"+ min);
+
+    }
+}
