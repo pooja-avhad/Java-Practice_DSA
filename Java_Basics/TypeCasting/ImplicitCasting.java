3c447872f3dc2f76026539fb1307b8af595255c0
pooja-avhad <avhadpooja408@gmail.com>
2026-03-17 01:07:38 +0530

    Added Implicit Type Casting Example

--- Java_Basics/TypeCasting/ImplicitCasting.java
+++ Java_Basics/TypeCasting/ImplicitCasting.java
@@ -1,1 +1,11 @@
+public class ImplicitCasting 
+{
+    public static void main(String[] args) 
+    {
+        //Implicit Casting
+        int a=3;
+        double b=a;
 
+        System.out.println(b);
+    }
+}
