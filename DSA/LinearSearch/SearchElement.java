51233c381b3864e81e8e5beff9fae0741f768d42
pooja-avhad <avhadpooja408@gmail.com>
2026-03-26 13:57:17 +0530

    Added Element Search

--- DSA/LinearSearch/SearchElement.java
+++ DSA/LinearSearch/SearchElement.java
@@ -1,1 +1,16 @@
+class SearchElement
+{
+    public static void main (String args[])
+    {
+        int arr[]={10,20,30};
+        int key=30;
 
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==key)
+            {
+              System.out.println("Element found at index " + i);
+            }
+        }
+     }
+ }
