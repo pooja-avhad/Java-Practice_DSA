ee774580f4a8e1ab43a5a2dd594251a1ffce01d1
pooja-avhad <avhadpooja408@gmail.com>
2026-04-01 01:29:51 +0530

    Added Bubble Sort implementation (descending order)

---
+++ DSA/Sorting/BS_DescendingOrder.java
@@ -0,0 +1,24 @@
+public class BS_DescendingOrder
+{
+    public static void main(String[] args) 
+    {
+        int arr[]={5,1,4,2};
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length-1;j++)
+            {
+                if(arr[j]<arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
+    
