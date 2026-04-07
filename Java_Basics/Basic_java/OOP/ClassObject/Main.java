775ff6222267a0facd63c10d1de3c18484617fab
pooja-avhad <avhadpooja408@gmail.com>
2026-04-07 23:34:39 +0530

    Added Java methods (functions) example

--- Java_Basics/Basic_java/OOP/ClassObject/Main.java
+++ Java_Basics/Basic_java/OOP/ClassObject/Main.java
@@ -1,1 +1,43 @@
+class Calculator {
 
+    // Method for addition
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method for subtraction
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Method for multiplication
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method for division
+    int divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero");
+            return 0;
+        }
+        return a / b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        int result1 = calc.add(10, 5);
+        int result2 = calc.subtract(10, 5);
+        int result3 = calc.multiply(10, 5);
+        int result4 = calc.divide(10, 5);
+
+        System.out.println("Addition: " + result1);
+        System.out.println("Subtraction: " + result2);
+        System.out.println("Multiplication: " + result3);
+        System.out.println("Division: " + result4);
+    }
+}
