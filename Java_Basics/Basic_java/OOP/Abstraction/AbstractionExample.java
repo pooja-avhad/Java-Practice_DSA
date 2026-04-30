2d27782fd5b0be4e8e3faf7abd24e76fb73cdb63
pooja-avhad <avhadpooja408@gmail.com>
2026-04-30 17:50:28 +0530

    Added Abstraction examples: Abstract class and Interface

--- Java_Basics/Basic_java/OOP/Abstraction/AbstractionExample.java
+++ Java_Basics/Basic_java/OOP/Abstraction/AbstractionExample.java
@@ -1,1 +1,22 @@
+  abstract class Animal 
+{
+    abstract void sound();   
 
+    void sleep() {           
+        System.out.println("Animal is sleeping");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class AbstractionExample {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.sleep();
+    }
+}
